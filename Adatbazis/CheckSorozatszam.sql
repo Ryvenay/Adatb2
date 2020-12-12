fd7e35892c02daacb1d4000f12580ff6c839565d
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-12-12 15:00:11 +0100

    checkSorozatszam sql kód hozzáadva

---
+++ Adatbazis/CheckSorozatszam.sql
@@ -0,0 +1,42 @@
+create or replace function sf_check_sorozatszam
+(
+    p_sorozatszam in gitarok.sorozatszam%TYPE
+)
+return int
+deterministic
+as
+    v_char char(1);
+    v_i int;
+begin
+    if p_rendszam is null then
+        return 0;
+    end if;
+
+    if length(trim(p_sorozatszam) < 4) then
+        return 0;
+    end if;
+    if length(trim(p_sorozatszam) > 10) then
+        return 0;
+    end if;
+
+
+    v_i := 1;
+    while v_i < length(trim(p_sorozatszam)) loop
+        v_char := substr(p_sorozatszam, v_i, 1);
+        exit when not (ascii('A') <= ascii(v_rendszam_char) and ascii(v_rendszam_char) <= ascii('Z'));
+        v_i = v_i+1;
+    end loop;
+
+    if v_i = length(trim(p_sorozatszam)) then
+        return 0;
+    end if;
+
+    while v_i < length(trim(p_sorozatszam)) loop
+        v_char := substr(p_sorozatszam, v_i, 1);
+        if not (ascii('0') <= ascii(v_rendszam_char) and ascii(v_rendszam_char) <= ascii('9')) then
+            return 0;            
+        end if;
+        v_i := v_i + 1;
+    end loop;
+    return 1;
+end;
