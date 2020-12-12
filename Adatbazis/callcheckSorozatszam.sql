cda022cbc227132edcd64aaf33e2f91364fad79e
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-12-12 15:13:41 +0100

    callcheck sorozatszám sql kód hozzáadva

---
+++ Adatbazis/callcheckSorozatszam.sql
@@ -0,0 +1,12 @@
+set serveroutput on;
+declare
+    v_call_sorozartszam int;
+    v_sorozatszam gitarok.sorozatszam%TYPE := 'N900007';
+begin
+    v_call_sorozartszam = sf_check_sorozatszam(v_sorozatszam);
+    IF v_call_sorozartszam = 1 THEN
+        DBMS_OUTPUT.PUT_LINE('A sorozatszam helyes: '||v_sorozatszam);
+    ELSE 
+        DBMS_OUTPUT.PUT_LINE('A sorozatszam helytelen: '||v_sorozatszam);    
+    END IF;
+end;
