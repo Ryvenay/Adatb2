0b8c96e26b323f1932d116e1905dfc3d9b2706f7
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-12-13 19:43:30 +0100

    insertGitarok sql kód javítva

--- Adatbazis/InsertGitarok.sql
+++ Adatbazis/InsertGitarok.sql
@@ -1,4 +1,4 @@
-create or replace procedure spInsert_gitarok(
+create or replace procedure sp_Insertgitarok(
     p_sorozatszam in gitarok.sorozatszam%TYPE,
     p_tipus in gitarok.tipus%TYPE,
     p_gyarto    in gyartok.nev%TYPE,
@@ -19,13 +19,13 @@ begin
         sp_insertGyartok(p_gyarto);
         v_gyarto_id := sf_getGyartoId(p_gyarto);
     end if;
-    v_check_sorozatszam := sf_check_sorozatszam(p_sorozatszam);
+    v_check_sorozatszam := sf_checksorozatszam(p_sorozatszam);
     if v_check_sorozatszam = 1 then
-        insert into autok
+        insert into gitarok
             (sorozatszam, tipus, gyarto_id, gyartas_datum, balkezes, erintok_szama, hangszedok)
         values
             (p_sorozatszam, p_tipus, v_gyarto_id, p_gyartas_datum, p_balkezes, p_erintok_szama, p_hangszedok);
         p_out_rowcnt := SQL%rowcount;
         commit;
     end if;
-end spInsert_autok;
+end;
